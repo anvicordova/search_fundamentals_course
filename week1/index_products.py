ac3a85d6322be2cd54694243420c34a737c9e404
Angela Cordova <anvicordova@gmail.com>
2023-02-10 21:04:32 +0000

    Add OpenSearch connection client

--- week1/index_products.py
+++ week1/index_products.py
@@ -84,8 +84,17 @@ def get_opensearch():
     host = 'localhost'
     port = 9200
     auth = ('admin', 'admin')
-    #### Step 2.a: Create a connection to OpenSearch
-    client = None
+
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+    )
+
     return client
 
 
